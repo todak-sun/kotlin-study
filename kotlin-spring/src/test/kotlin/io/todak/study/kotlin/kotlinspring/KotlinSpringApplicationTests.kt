3e6fbda2a0908b7ec57c02591b94bad5d9f69894
todak-sun <tjsdydwn@gmail.com>
2022-06-22 22:07:53 +0900

    kotlin-spring 강의시작

---
+++ kotlin-spring/src/test/kotlin/io/todak/study/kotlin/kotlinspring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.todak.study.kotlin.kotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
