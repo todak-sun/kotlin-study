29d632cb05f7e7eac4e19b7326e3e786f77f7852
todak-sun <tjsdydwn@gmail.com>
2022-07-02 21:35:01 +0900

    생성자 정리

---
+++ kotlin-in-action/src/io/study/kotlininaction/_04/ClassInit.kt
@@ -0,0 +1,29 @@
+package io.study.kotlininaction._04
+
+
+// 간단한 클래스를 선언하는 방법
+
+class User(val nickname: String)
+
+/**
+ * 클래스 이름 뒤에 오는 괄호로 둘러싸인 코드를 주 생성자(primary constructor)라고 부른다.
+ */
+
+class User2 constructor(_nickname: String) {
+    val nickname: String
+
+    init {
+        nickname = _nickname
+    }
+}
+
+class User3 {
+    constructor(name: String) {
+
+    }
+
+    constructor(name: String, age: Int) {
+
+    }
+}
+
