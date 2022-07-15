4b604f2c35eb51be94cca3fd837dda3e1d9d0744
todak-sun <tjsdydwn@gmail.com>
2022-07-15 22:11:05 +0900

    lambda 끝

--- kotlin-in-action/src/io/study/kotlininaction/_05/LambdaWithReceiver.kt
+++ kotlin-in-action/src/io/study/kotlininaction/_05/LambdaWithReceiver.kt
@@ -8,3 +8,27 @@ fun alphabet(): String {
     result.append("\nNow I know alphabet!")
     return result.toString()
 }
+
+
+fun alphabetWith(): String {
+    val stringBuilder = StringBuilder()
+    return with(stringBuilder) {
+        for (letter in 'A'..'Z') {
+            this.append(letter)
+        }
+        append("\nNow I know the alphabet!")
+        this.toString()
+    }
+}
+
+fun alphabetApply() = StringBuilder().apply {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append("\nNow I know the alphabet!")
+}.toString()
+
+fun main() {
+    alphabet()
+    alphabetWith()
+}
