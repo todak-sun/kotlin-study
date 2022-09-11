37c84153caadb8cb59780307c5e8362e655d00ba
todak-sun <tjsdydwn@gmail.com>
2022-09-11 22:58:38 +0900

    공부 시작~

--- library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -3,6 +3,7 @@ package com.group.libraryapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 실행되는지 확인중
 @SpringBootApplication
 public class LibraryAppApplication {
 
