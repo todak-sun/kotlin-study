298e71103dd803b6c6c95eea0fdda60e06f991ae
todak-sun <tjsdydwn@gmail.com>
2022-09-27 20:03:46 +0900

    마무리

--- library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepositoryCustomImpl.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepositoryCustomImpl.kt
@@ -1,14 +1,23 @@
 package com.group.libraryapp.domain.book
 
+import com.group.libraryapp.domain.book.QBook.book
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.dto.book.response.QBookStatResponse
 import com.querydsl.jpa.impl.JPAQueryFactory
 
 class BookRepositoryCustomImpl(
     private val qf: JPAQueryFactory
 ) : BookRepositoryCustom {
     override fun getStats(): List<BookStatResponse> {
-//        @Query("SELECT new com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) FROM Book b GROUP BY b.type")
 
+        return qf.select(
+            QBookStatResponse(
+                book.type,
+                book.id.count()
+            )
+        ).from(book)
+            .groupBy(book.type)
+            .fetch()
     }
 
 
