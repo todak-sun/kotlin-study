cf6a9072b70a2bf0b3e0819ac1ed666418b1bff1
todak-sun <tjsdydwn@gmail.com>
2022-09-28 23:50:02 +0900

    sftp 테스트 계속

--- sftp-testing/src/test/kotlin/io/todak/sftptesting/testhelper/WithTestContainer.kt
+++ sftp-testing/src/test/kotlin/io/todak/sftptesting/testhelper/WithTestContainer.kt
@@ -15,8 +15,8 @@ class WithTestContainer {
 
     companion object {
 
-        private val username: String = "sftpuser"
-        private val password: String = "strong!@#pasword"
+        private const val username: String = "sftpuser"
+        private const val password: String = "strong!@#pasword"
 
         @Container
         private val sftpServierContainer: GenericContainer<*> =
@@ -25,27 +25,42 @@ class WithTestContainer {
                 .withCommand("${username}:${password}:1001")
 
         init {
+            System.setProperty("sftp.host", "localhost")
+            System.setProperty("sftp.username", username)
+            System.setProperty("sftp.password", password)
             sftpServierContainer.start()
-            println("mapped port : ${sftpServierContainer.getMappedPort(22)}")
+            System.setProperty("sftp.port", sftpServierContainer.getMappedPort(22).toString())
         }
 
         @DynamicPropertySource
         @JvmStatic
         fun registryDynamicProperties(registry: DynamicPropertyRegistry) {
             registry.add("sftp.host") { "localhost" }
+            registry.add("sftp.password") { password }
+            registry.add("sftp.username") { username }
         }
     }
 
     @Test
     fun test() {
         val jSch = JSch()
-        val session = jSch.getSession("sftpuser", "localhost", sftpServierContainer.getMappedPort(22))
-        session.setPassword("strong!@#pasword")
-        session.setConfig("StrictHostKeyChecking", "no")
-        session.connect()
+        val session = jSch.getSession(
+            System.getProperty("sftp.username"),
+            System.getProperty("sftp.host"),
+            System.getProperty("sftp.port").toInt()
+        )
 
+        session.setPassword(System.getProperty("sftp.password"))
+        session.setConfig("StrictHostKeyChecking", "no")
+
+        session.connect()
         assertTrue(session.isConnected)
 
+        val channel = session.openChannel("sftp")
+        channel.connect()
+        assertTrue(channel.isConnected)
+
+        channel.disconnect()
         session.disconnect()
     }
 
